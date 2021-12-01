3edd8123ea40cd9bea717eb912d67d2abf9d8545
anthony-Chk <94873096+anthony-Chk@users.noreply.github.com>
2021-12-01 16:13:01 -0500

    Add files via upload

---
+++ AccountCreation.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AccountCreation extends JFrame{
+    private JLabel logoImage;
+    private JButton profileButton;
+    private JButton homeButton;
+    private JButton MenuButton;
+    private JPanel mainPanel;
+    private JTextField nameField;
+    private JTextField emailField;
+    private JTextField phoneField;
+    private JPasswordField passField2;
+    private JPasswordField passField;
+    private JTextField cardField;
+    private JTextField expField;
+    private JTextField cvvfield;
+    private JTextField addressField;
+    private JLabel errorField;
+    private JButton saveButton;
+
+    public AccountCreation(String name) {
+        super(name);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setVisible(true);
+        this.setMinimumSize(new Dimension(1080, 720));
+
+        this.pack();
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkInformation();
+
+            }
+        });
+
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame menu = new Menu("Menu");
+                dispose();
+            }
+        });
+        MenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame fullMenu = new FullMenu("Full Menu");
+                dispose();
+            }
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+    public void checkInformation() {
+        String tempString;
+        char[] tempPass;
+
+        errorField.setText("");
+
+        tempString = emailField.getText();
+        if (tempString.indexOf('@') == -1) {
+            errorField.setText("Invalid email address");
+        }
+
+        tempString = phoneField.getText();
+        if (tempString.length()>10 || tempString.length()<10) {
+            errorField.setText("Invalid phone number");
+        }
+
+        tempPass = passField.getPassword();
+        for (int i=0; i<tempPass.length+1; i++) {
+            if (tempPass[i] != passField2.getPassword()[i]) {
+                errorField.setText("Passwords do not match");
+            }
+        }
+
+    }
+
+
+    public void saveInformation() {
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new AccountCreation("Account Creation");
+    }
+}
---
+++ Sides.java
@@ -0,0 +1,152 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Sides extends JFrame{
+    private JPanel mainPanel;
+    private JLabel logoImage;
+    private JButton profileButton;
+    private JButton homeButton;
+    private JButton menuButton;
+    private JButton contactUsButton;
+    private JButton sidesButton;
+    private JButton pizzaButton;
+    private JButton beveragesButton;
+    private JPanel pizzaPanel;
+    private JButton Breadsticks;
+    private JButton BreakstickBites;
+    private JButton BCCC;
+    private JLabel breadstickImage;
+    private JLabel Cookie;
+    private JButton P1;
+    private JButton P2;
+    private JButton P3;
+    private JLabel BreadbitesImage;
+    private JPanel quantityTab;
+    private JLabel quantityText;
+    private JTextArea quantity2;
+    private JButton plusquantity2;
+    private JButton minusquantity2;
+    private JTextArea Quantity1;
+    private JButton plusquantity1;
+    private JButton minusquantity1;
+    private JRadioButton marinara1;
+    private JRadioButton marinara2;
+    private JButton NextPage;
+    private JTextArea quantity3;
+    private JButton plusquantity3;
+    private JButton minusquantity3;
+
+    public Sides(String name) {
+        super(name);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setVisible(true);
+        this.setMinimumSize(new Dimension(1080, 720));
+
+        this.pack();
+
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame menu = new Menu("Menu");
+                dispose();
+            }
+        });
+        menuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame fullMenu = new FullMenu("Full Menu");
+                dispose();
+            }
+        });
+        contactUsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame contact = new Contact("Contact Us");
+                dispose();
+            }
+        });
+
+        pizzaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame pizza = new PizzaCreator("Pizza menu");
+                dispose();
+            }
+        });
+        sidesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame sides = new Sides("side items");
+                dispose();
+            }
+        });
+
+        final int[] quantity = {0};
+        this.Quantity1.setText(String.valueOf(quantity[0]));
+        this.quantity2.setText(String.valueOf(quantity[0]));
+        this.quantity3.setText(String.valueOf(quantity[0]));
+        this.minusquantity1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                quantity[0] = quantity[0] -1;
+                if (quantity[0]<0) {
+                    quantity[0] = 0;
+                }
+                Quantity1.setText(String.valueOf(quantity[0]));
+            }
+        });
+        this.plusquantity1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quantity[0] = quantity[0] +1;
+                Quantity1.setText(String.valueOf(quantity[0]));
+            }
+        });
+        this.minusquantity2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                quantity[0] = quantity[0] -1;
+                if (quantity[0]<0) {
+                    quantity[0] = 0;
+                }
+                quantity2.setText(String.valueOf(quantity[0]));
+            }
+        });
+        this.plusquantity2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quantity[0] = quantity[0] +1;
+                quantity2.setText(String.valueOf(quantity[0]));
+            }
+        });
+        this.minusquantity3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                quantity[0] = quantity[0] -1;
+                if (quantity[0]<0) {
+                    quantity[0] = 0;
+                }
+                quantity3.setText(String.valueOf(quantity[0]));
+            }
+        });
+        this.plusquantity3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quantity[0] = quantity[0] +1;
+                quantity3.setText(String.valueOf(quantity[0]));
+            }
+        });
+        profileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame accountCreation = new Profile("Account Creator");
+                dispose();
+            }
+        });
+    }
+    public static void main(String[] args)
+    {
+        JFrame sides = new Sides("side items");
+    }
+}
